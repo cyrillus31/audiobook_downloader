ea7a2b09733179fbd07feb2648ef9bb2024695dc
Kirill F <kirill.olegovich31@gmail.com>
2023-01-10 01:18:09 +0300

    a few cosmetic changes to code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 """
-LOGIC
+LOGIC of the main loop
 1. Open first page
 2. Create soup
 3. Find all books on the page
@@ -17,7 +16,7 @@ import os
 input ("""
 UPDATED VERSION 11/23/22
 
-____________________!!!!!!!!!!THIS IS THE NEW INFORMATION!!!!!!!!!!_________________
+____________________!!!!!!!!!!THIS IS NEW INFORMATION!!!!!!!!!!_________________
 (in the previous version the link was with "s" at the end https://fulllengthaudiobooks.com/)
 
 
@@ -41,23 +40,21 @@ booknumber = ""
 totalpages = ""
 search = input ("What do you want to search for?\n").replace(" ", "+")
 
-
-def all_books_on_page_func(pagenumber):
+# This function prodcues a list of all the books and writes a total number of available pages to a global variable
+def all_books_on_page_func(pagenumber: int) -> list:
     global search
 
-    #     search = "xenos"
-
-    # get a response from the page
+    # Get a response from the page
     response = requests.get("https://fulllengthaudiobook.com/page/" + str(pagenumber) + "/?s=" + search)
-    print ("https://fulllengthaudiobook.com/page/" + str(pagenumber) + "/?s=" + search)
-    # create a soup
+    # print ("https://fulllengthaudiobook.com/page/" + str(pagenumber) + "/?s=" + search)
+    print("should be shown one")
+    # Create soup
     soup = bs4.BeautifulSoup(response.text, "lxml")
 
-    # find a class that contains each book. name of the class starts with a dot and all spaces are dots
+    # Find a class that contains each book. Name of the class starts with a dot and all spaces are dots
     all_books_on_page = soup.select(".entry.clearfix")
-    # print (len(all_books_on_page))
 
-    # searching for the total number of pages for this particular search
+    # Looking for a total number of pages available in this particular search and assign to a global variable
     global totalpages
     if soup.select(".page-numbers") == []:
         totalpages = 1
@@ -79,7 +76,7 @@ def booknumber_on_the_page(all_books_on_page):
     global pagenumber
     global totalpages
     list_of_titles_per_page = []
-    list_of_tuple_titles = []
+    list_of_enumerated_titles = []
     for book in all_books_on_page:
         if len(book.select("p")[0].text) == 1:
             list_of_titles_per_page.append(book.select("p")[1].text)
@@ -88,15 +85,16 @@ def booknumber_on_the_page(all_books_on_page):
 
     # print (list_of_titles_per_page)
     for a, b in enumerate(list_of_titles_per_page, start=1):
-        list_of_tuple_titles.append((a, b))
+        list_of_enumerated_titles.append((a, b))
 
-    print("\nThe page number is {} of {}".format(mypagenumber, totalpages))
-    print("Amount of books on the page is {}\n".format(len(list_of_tuple_titles)))
-    print("0) Turn the page forward")
-    print("00) Turn the page backward")
-    print("000) Change the search\n")
+    # Following statments explain navigation for the user
+    print("\nThe page number is {} of {}\n".format(mypagenumber, totalpages)+
+    "Amount of books on the page is {}\n".format(len(list_of_enumerated_titles))+
+    "0) Turn the page forward\n"+
+    "00) Turn the page backward\n"+
+    "000) Change the search\n")
 
-    for a, b in list_of_tuple_titles:
+    for a, b in list_of_enumerated_titles:
         print(str(a) + ")", b)
 
 
